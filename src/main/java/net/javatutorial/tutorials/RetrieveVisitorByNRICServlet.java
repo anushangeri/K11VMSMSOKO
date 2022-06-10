584deb7fad41b9cbadbd217b70eca7b82439cc6d
anushangeri <anushangeri@gmail.com>
2022-06-10 14:26:54 -0400

    test

--- src/main/java/net/javatutorial/tutorials/RetrieveVisitorByNRICServlet.java
+++ src/main/java/net/javatutorial/tutorials/RetrieveVisitorByNRICServlet.java
@@ -1,38 +1,22 @@
 package net.javatutorial.tutorials;
 
 import java.io.IOException;
-import java.net.URL;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Locale;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
-import org.joda.time.format.DateTimeFormatter;
-
-import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.spreadsheet.CustomElementCollection;
-import com.google.gdata.data.spreadsheet.ListEntry;
-import com.google.gdata.data.spreadsheet.ListFeed;
 
 import net.javatutorial.DAO.DropdownListManagerDAO;
 import net.javatutorial.DAO.SiteManagerDAO;
 import net.javatutorial.DAO.VMSManagerDAO;
-import net.javatutorial.entity.Visitor;
 import net.javatutorial.entity.Dropdown;
 import net.javatutorial.entity.Site;
+import net.javatutorial.entity.Visitor;
 
 /**
  * Servlet implementation class RetrieveVisitorByNRICServlet
@@ -46,11 +30,10 @@ public class RetrieveVisitorByNRICServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String usertype = (String) request.getSession(false).getAttribute("usertype");
 		String idNo = (String) request.getSession(false).getAttribute("idNo");
-		String idType = (String) request.getSession(false).getAttribute("idType");
 		
 		//from client login view
-		String idNoFromClient = request.getParameter("idNo");
-		String idTypeFromClient = request.getParameter("idType");
+		String vmsId = request.getParameter("vmsId");
+		String status = request.getParameter("status");
 		
 		ArrayList<Visitor> vList = null;
 		Visitor v = null;
@@ -64,10 +47,8 @@ public class RetrieveVisitorByNRICServlet extends HttpServlet {
 			}
 		}
 		else {
-			vList = VMSManagerDAO.retrieveByNameIDPopulate(idNoFromClient);
-			if(vList != null && vList.size() > 0) {
-				v = vList.get(0);
-			}
+			v = VMSManagerDAO.retrieveByVmsId(vmsId);
+			
 		}
 		ArrayList<Site> siteDropdown = SiteManagerDAO.retrieveAll();
 		ArrayList<Dropdown> visitPurpose = DropdownListManagerDAO.retrieveByDropdownKey("VISIT_PURPOSE");
@@ -75,6 +56,7 @@ public class RetrieveVisitorByNRICServlet extends HttpServlet {
 		request.setAttribute("visitorLatRec", v);
 		request.setAttribute("siteDropdown", siteDropdown);
 		request.setAttribute("visitPurpose", visitPurpose);
+		request.setAttribute("status", status);
         RequestDispatcher rd = request.getRequestDispatcher("addVisitor.jsp");
         rd.forward(request, response);
 	}
